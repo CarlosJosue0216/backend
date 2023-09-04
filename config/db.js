7378ee9ddb01c3ecaeb526fdd01f72e2f155a7f5
unknown <carlosjlc0216@gmail.com>
2023-09-03 23:06:50 -0600

    update env

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   host: process.env.DBHOST,
   user: process.env.DBUSER,
   password:process.env.DBPASS,
-  database: 'backendproyecto',
+  database: process.env.DBSCHEMA,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
